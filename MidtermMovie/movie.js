40b14d6a1bd88c8a32f0428f2fb3365b9dc5a42f
gabgab017 <121985892+gabgab017@users.noreply.github.com>
2023-02-22 18:22:25 -0800

    Create movie.js

---
+++ MidtermMovie/movie.js
@@ -0,0 +1,63 @@
+const apiKey = '30d6e6181e02f426df1cb8910eeddf36';
+const trendingApiUrl = `https://api.themoviedb.org/3/trending/movie/week?api_key=${apiKey}`;
+
+// get a reference to the search form, search input, and movie container
+const searchForm = document.getElementById('search-container');
+const searchInput = document.getElementById('search-input');
+const movieContainer = document.getElementById('movies-container');
+
+// create a template for the movie tile
+const movieTileTemplate = ({ title, release_date: releaseDate, overview: description, genre_ids: genreIds, poster_path: posterPath }) => {
+  const imageUrl = `https://image.tmdb.org/t/p/w500${posterPath}`;
+  const genres = genreIds.join(', ');
+
+  return `
+    <div class="movie-tile">
+      <img src="${imageUrl}" alt="${title}">
+      <h3>${title}</h3>
+      <p>Released: ${releaseDate.substring(0, 4)}</p>
+      <p>Genres: ${genres}</p>
+      <p>${description}</p>
+    </div>
+  `;
+};
+
+// fetch data from the API and update the HTML with the movie data
+const fetchMovies = (url) => {
+  console.log('Fetching movies from URL:', url);
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const movies = data.results;
+
+      // clear the previous movie tiles
+      movieContainer.innerHTML = '';
+
+      movies.forEach(movie => {
+        // create a new movie tile and fill in the data
+        const movieTile = document.createElement('div');
+        movieTile.classList.add('movie-tile');
+        movieTile.innerHTML = movieTileTemplate(movie);
+
+        // add the movie tile to the container
+        movieContainer.appendChild(movieTile);
+      });
+    })
+    .catch(error => {
+      console.error('Error fetching movies:', error);
+    });
+};
+
+// fetch the initial list of movies (trending movies of the week)
+fetchMovies(trendingApiUrl);
+
+// listen for search form submissions
+searchForm.addEventListener('submit', event => {
+  event.preventDefault();
+  const searchQuery = searchInput.value.trim();
+
+  if (searchQuery.length >= 3) {
+    const searchApiUrl = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${searchQuery}`;
+    fetchMovies(searchApiUrl);
+  }
+});
